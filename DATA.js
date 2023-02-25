77c01965a678cc9ed48f0ac178b4c1a058b366e2
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-25 14:32:25 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "bd7da571-e5f1-465a-b930-d146182df07d",
+    "home_feed": [
+        {
+            "id": "46b744b6-56ac-40ae-bdad-58da4e9e0503",
+            "artist_name": "trade",
+            "home_carousel": [
+                {
+                    "id": "c1fd643f-dc0b-4735-a92d-503ee91c6fe5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/339.png"
+                },
+                {
+                    "id": "57112a3b-c70a-4c85-9c89-6458254c7f30",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/25.png"
+                },
+                {
+                    "id": "fdab225d-b29b-4877-bb8e-d21cd20ce561",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/166.png"
+                },
+                {
+                    "id": "8a9c8e05-2cb8-4e0a-96f9-1a952a3ea763",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/286.png"
+                },
+                {
+                    "id": "0dcedb3e-eaf8-4a30-880c-552b8a52e7b9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/165.png"
+                }
+            ],
+            "art_caption": "likely",
+            "art_description": "White approach wind ever."
+        },
+        {
+            "id": "89ce9924-56d6-4a83-a080-ad116da5deb9",
+            "artist_name": "guy",
+            "home_carousel": [
+                {
+                    "id": "246cec2e-d431-437d-965f-a21ee524aadb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/205.png"
+                },
+                {
+                    "id": "72d3d4d1-2dbf-468e-b311-75ed241f1ef3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/228.png"
+                },
+                {
+                    "id": "ba8260e4-a770-4576-bdfc-6a1980c3ed92",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/184.png"
+                },
+                {
+                    "id": "5f785e8e-271b-4d23-a46f-3e7134f6b16b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/128.png"
+                },
+                {
+                    "id": "e1d76e6c-b7b2-408c-a879-f01af9e7c0bd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/43.png"
+                }
+            ],
+            "art_caption": "blue",
+            "art_description": "Congress career once sing sense. Ten item he Mr effect reflect adult."
+        },
+        {
+            "id": "1f505b92-b348-44d2-9627-72713c68cc96",
+            "artist_name": "will",
+            "home_carousel": [
+                {
+                    "id": "ae3b7be5-5ccc-42a7-99d7-c08d950dd3d3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/109.png"
+                },
+                {
+                    "id": "9e262065-7da6-4de8-bccc-3bcb7e51c5b3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/66.png"
+                },
+                {
+                    "id": "6c924980-1d37-4fe1-838f-776780078a10",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/121.png"
+                },
+                {
+                    "id": "f5e58234-3e14-4270-9498-798bcbc81c65",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/352.png"
+                },
+                {
+                    "id": "ae10bcbb-be03-4ff5-a59c-713896c085a8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/72.png"
+                }
+            ],
+            "art_caption": "usually",
+            "art_description": "Medical yard surface real rich recent. Create job project grow."
+        },
+        {
+            "id": "704b3901-633d-401d-ae9a-d3e549daa51f",
+            "artist_name": "we",
+            "home_carousel": [
+                {
+                    "id": "48b067f7-5cc7-4ed3-80cd-b41d942fd8a7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/266.png"
+                },
+                {
+                    "id": "fe20ac8a-82fb-4c98-ac75-99514c22c433",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/354.png"
+                },
+                {
+                    "id": "c782f27c-2bc5-4bdb-8868-f1f8f8fe1886",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/126.png"
+                },
+                {
+                    "id": "9e1b0c43-44c9-4440-bb53-6af6aef73bae",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/38.png"
+                },
+                {
+                    "id": "37bbd45f-60e4-40fd-ab8a-b1253d450b2c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/187.png"
+                }
+            ],
+            "art_caption": "between",
+            "art_description": "Attack avoid offer a less sport recognize. Within thousand small city. Cause capital large election."
+        },
+        {
+            "id": "096b2a3f-d409-4217-8de1-14d8b99937d5",
+            "artist_name": "project",
+            "home_carousel": [
+                {
+                    "id": "896638f6-4c44-451e-9478-cf0b9228dea8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/258.png"
+                },
+                {
+                    "id": "dcbbc018-1775-4570-b0e8-c050cb7d42a7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/271.png"
+                },
+                {
+                    "id": "bf04b5c3-a8f0-4c85-a485-560282039e6f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/252.png"
+                },
+                {
+                    "id": "476b9e5e-4a7a-48b1-93ac-f9353b6f98f3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/123.png"
+                },
+                {
+                    "id": "dd22bcf8-244d-43c8-a808-6db3276249e5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/195.png"
+                }
+            ],
+            "art_caption": "well",
+            "art_description": "Enough oil run page. Traditional tend short fish may again. Film scientist he night plant."
+        }
+    ]
+}
